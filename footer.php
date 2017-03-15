432b99ace3e38f55adf14a36d990153f535aed56
Vibhor <vibhorporwal99@gmail.com>
2017-03-16 03:51:16 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+<div id="footer">
+  Legal Adviser
+</div>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <title>NYAYAbandhu</title>
+  <link href="public.css" media="all" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div id="header">
+    <h1>NYAYAbandhu</h1>
+  </div>
+  <div id="main">
