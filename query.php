1861e256012ee9152e408011a380d2949de0e9cb
Vibhor <vibhorporwal99@gmail.com>
2017-03-16 21:50:16 +0530

    Add files via upload

---
+++ query.php
@@ -0,0 +1,31 @@
+<?php include("header.php"); ?>
+<table id="structure">
+  <tr>
+    <td id="navigation">
+
+      Quick Links:<br><br>
+        <a href="http://supremecourtofindia.nic.in/" target="_blank">Supreme Court of India</a><br><br>
+        <a href="https://www.supremecourt.uk/current-cases/" target="_blank">Recent Cases</a><br><br>
+        <a href="http://topyaps.com/top-10-famous-court-cases-in-india" target="_blank">Some famous Indian Cases</a><br><br>
+        <a href="http://rti.gov.in/" target="_blank">Right To Information</a><br><br>
+        <a href="http://nalsa.gov.in/lsams/nologin/applicationFiling.action?requestLocale=en" target="_blank">NALSA Legal Service</a><br><br>
+
+    </td>
+    <td id="page">
+      <h2 align="center"><u>NYAYAbandhu</u></h2>
+      <p align="center"><font face="Chiller" size="5px">Looking for help about your case,here is your friend.</font></p>
+      <form method="post" action="ind_action.php">
+      Case: <input type="text" name="cname"><br><br>Name:<input type="text" name="name"><br><br>
+
+        <input type="submit" name="submit"><br><br>
+        <a href="https://www.wdl.org/en/item/2672/" target="_blank"><center><div id="img"><img align="center" src="cons.jpg" width="450px"></div></center></a>
+
+
+
+
+      </form>
+
+    </td>
+  </tr>
+</table>
+<?php include("footer.php"); ?>
