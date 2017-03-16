baa1db79fdd0fe150c108262637136ed1d66a1a8
Vibhor <vibhorporwal99@gmail.com>
2017-03-16 21:45:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,25 +2,20 @@
 <table id="structure">
   <tr>
     <td id="navigation">
-
+      Quick Links:<br><br>
         <a href="http://supremecourtofindia.nic.in/" target="_blank">Supreme Court of India</a><br><br>
         <a href="https://www.supremecourt.uk/current-cases/" target="_blank">Recent Cases</a><br><br>
         <a href="http://topyaps.com/top-10-famous-court-cases-in-india" target="_blank">Some famous Indian Cases</a><br><br>
+        <a href="http://rti.gov.in/" target="_blank">Right To Information</a><br><br>
+        <a href="http://nalsa.gov.in/lsams/nologin/applicationFiling.action?requestLocale=en" target="_blank">NALSA Legal Service</a><br><br>
 
     </td>
-    <td id="page">
-      <h2 align="center"><u>NYAYAbandhu</u></h2>
-      <p align="center"><font face="Chiller" size="5px">Looking for help about your case,here is your friend.</font></p>
-      <form method="post" action="ind_action.php">
-      Case: <input type="text" name="cname"><br><br>Name:<input type="text" name="name"><br><br>
+    <td align="center" id="page">
+      <h2><u>NYAYAbandhu</u></h2>
+      <img src="logo.png"><br><br>
+      <font size="5px" face="Broadway">Tired of your lawyer,we are here to aide you on legal matters.<br><br>
+      <a href="query.php">Want Help</a></font>
 
-        <input type="submit" name="submit"><br><br>
-        <a href="https://www.wdl.org/en/item/2672/" target="_blank"><center><img align="center" src="cons.jpg" width="450px"></center></a>
-
-
-
-
-      </form>
 
     </td>
   </tr>
