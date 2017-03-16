9a81d2ff4b006507855de8f7af1629be05148da9
Vibhor <vibhorporwal99@gmail.com>
2017-03-16 21:46:58 +0530

    Update ind_action.php

--- ind_action.php
+++ ind_action.php
@@ -3,12 +3,16 @@
   <tr>
     <td id="navigation">
 
-      <a href="http://supremecourtofindia.nic.in/" target="_blank">Supreme Court of India</a><br><br>
-      <a href="https://www.supremecourt.uk/current-cases/" target="_blank">Recent Cases</a><br><br>
-      <a href="http://topyaps.com/top-10-famous-court-cases-in-india" target="_blank">Some famous Indian Cases</a><br><br>
+      Quick Links:<br><br>
+        <a href="http://supremecourtofindia.nic.in/" target="_blank">Supreme Court of India</a><br><br>
+        <a href="https://www.supremecourt.uk/current-cases/" target="_blank">Recent Cases</a><br><br>
+        <a href="http://topyaps.com/top-10-famous-court-cases-in-india" target="_blank">Some famous Indian Cases</a><br><br>
+        <a href="http://rti.gov.in/" target="_blank">Right To Information</a><br><br>
+        <a href="http://nalsa.gov.in/lsams/nologin/applicationFiling.action?requestLocale=en" target="_blank">NALSA Legal Service</a><br><br>
 
     </td>
     <td id="page">
+      <div id="imgl"><img align="right" src="logo.png" width="500px"></div>
       <h2><u>NYAYAbandhu</u></h2>
 
         Similar Cases:<br>
@@ -23,8 +27,8 @@
       <form method="post" action="mailto.php">
         Email: <input type="text" name="email"><br><br>State: <input type="text" name="state"><br><br>
         Case Description:<br><br>
-        <textarea name="desc" rows="5" columns="3"></textarea><br><br>
-        Mail an expert:<br><br>
+        <textarea name="desc" rows="5" columns="7"></textarea><br><br>
+        
         <input type="submit" name="Mail">
       </form>
 
