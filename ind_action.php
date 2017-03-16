abbf689fba6366d17f7dfce0462a68a5894600ce
Vibhor <vibhorporwal99@gmail.com>
2017-03-16 17:23:59 +0530

    Update ind_action.php

--- ind_action.php
+++ ind_action.php
@@ -8,19 +8,21 @@
       <a href="http://topyaps.com/top-10-famous-court-cases-in-india" target="_blank">Some famous Indian Cases</a><br><br>
 
     </td>
-    <td align="center" id="page">
+    <td id="page">
       <h2>NYAYAbandhu</h2>
 
         Similar Cases:<br>
+        <ul>
           <?php
           $case=$_POST['cname'];
-            echo "<a target=_blank href=https://indiankanoon.org/search/?formInput={$case}%20%20+doctypes:judgments>Some previous ".$case." cases in India</a>"."<br>"."<br>";
-            echo "<a target=_blank href=https://indiankanoon.org/search/?formInput={$case}%20%20+doctypes:laws>Laws in Indian Constitution related to "."$case"." </a>"."<br>"."<br>";
+            echo "<li>"."<a target=_blank href=https://indiankanoon.org/search/?formInput={$case}%20%20+doctypes:judgments>Some previous ".$case." cases in India</a>"."<br>"."<br>";
+            echo "<li>"."<a target=_blank href=https://indiankanoon.org/search/?formInput={$case}%20%20+doctypes:laws>Laws in Indian Constitution related to "."$case"." </a>"."<br>"."<br>";
           ?>
+        </ul>
       <form method="post" action="mailto.php">
-        Email: <input type="text" name="email">  State: <input type="text" name="state"><br><br>
-        Case Description:<br>
-        <textarea name="desc" rows="4" columns="6"></textarea><br><br>
+        Email: <input type="text" name="email"><br><br>State: <input type="text" name="state"><br><br>
+        Case Description:<br><br>
+        <textarea name="desc" rows="5" columns="3"></textarea><br><br>
         Mail an expert:<br><br>
         <input type="submit" name="Mail">
       </form>
