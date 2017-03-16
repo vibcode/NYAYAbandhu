fbbb9110859f127f94e0f6b6ab9fdfbce583091a
Vibhor <vibhorporwal99@gmail.com>
2017-03-16 16:46:49 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include("header.php"); ?>
+<table id="structure">
+  <tr>
+    <td id="navigation">
+
+        <a href="http://supremecourtofindia.nic.in/" target="_blank">Supreme Court of India</a><br><br>
+        <a href="https://www.supremecourt.uk/current-cases/" target="_blank">Recent Cases</a><br><br>
+        <a href="http://topyaps.com/top-10-famous-court-cases-in-india" target="_blank">Some famous Indian Cases</a><br><br>
+
+    </td>
+    <td id="page">
+      <h2 align="center">NYAYAbandhu</h2>
+      <p align="center">Looking for help about your case,here is your friend.</p>
+      <form method="post" action="ind_action.php">
+      Case: <input type="text" name="cname"><br><br>Name:<input type="text" name="name"><br><br>
+
+        <input type="submit" name="submit"><br><br>
+        <a href="https://www.wdl.org/en/item/2672/" target="_blank"><center><img align="center" src="cons.jpg" width="450px"></center></a>
+
+
+
+
+      </form>
+
+    </td>
+  </tr>
+</table>
+<?php include("footer.php"); ?>
