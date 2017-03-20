c14749b863efcb3dec442049ca21bf1948e91fe9
Vibhor <vibhorporwal99@gmail.com>
2017-03-20 13:13:17 +0530

    Update main_my.php

--- main_my.php
+++ main_my.php
@@ -4,7 +4,7 @@
     <td id="navigation">
       <font face=Broadway>Quick Links:</font><br><br>
         <a href="http://supremecourtofindia.nic.in/" target="_blank">Supreme Court of India</a><br><br>
-        <a href="https://www.supremecourt.uk/current-cases/" target="_blank">Recent Cases</a><br><br>
+        <a href="http://www.advocatekhoj.com/library/judgments/announcement.php" target="_blank">Recent Cases</a><br><br>
         <a href="http://topyaps.com/top-10-famous-court-cases-in-india" target="_blank">Some famous Indian Cases</a><br><br>
         <a href="http://rti.gov.in/" target="_blank">Right To Information</a><br><br>
         <a href="http://nalsa.gov.in/lsams/nologin/applicationFiling.action?requestLocale=en" target="_blank">NALSA Legal Service</a><br><br>
